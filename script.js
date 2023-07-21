024d38968f8543c4bda8ec493ce5c8d120b078d9
Aevann2 <135237318+Aevann2@users.noreply.github.com>
2023-07-21 16:16:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-var url = new URL(window.location.href);
-var p = url.searchParams.get("p");
-var c = url.searchParams.get("c");
-if(p !== null) {
-    window.location.href='https://rdrama.net/post/'+p;
-}
-else if(c !== null) {
-    window.location.href='https://rdrama.net/comment/'+c+'/?context=5#context';
-}
-else {
-    //probably a better way to do this
-}
+location.href = 'https://rdrama.net/'+location.href;
